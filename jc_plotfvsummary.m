1e5ca643ada0f9dfe6db103b5189a1a0957b1ec6
Joanne <jcheung2009@gmail.com>
2015-08-06 18:22:34 -0700

    modified jc_plotfvsummary to fix CV norm

--- jc_plotfvsummary.m
+++ jc_plotfvsummary.m
@@ -1,15 +1,43 @@
-function jc_plotfvsummary(fv_syll_sal, fv_syll_cond,marker,linecolor);
+function jc_plotfvsummary(fv_syll_sal, fv_syll_cond,marker,linecolor,excludewashin);
 %fv_syll from jc_findwnote5
+%generate summary changes in pitch, entropy, volume, and pitch cv
+%(normalized and raw)
+
+
+if iscell(fv_syll_sal)
+    tb_sal=[];
+    fv_syll = [];
+    for i = 1:length(fv_syll_sal)
+        tb_sal = [tb_sal; jc_tb([fv_syll_sal{i}(:).datenm]',7,0)];
+        fv_syll = [fv_syll fv_syll_sal{i}];
+    end
+    fv_syll_sal = fv_syll;
+end
+if iscell(fv_syll_cond)
+    tb_cond=[];
+    fv_syll = [];
+    for i = 1:length(fv_syll_cond)
+        tb_cond = [tb_cond; jc_tb([fv_syll_cond{i}(:).datenm]',7,0)];
+        fv_syll =[fv_syll fv_syll_cond{i}];
+    end
+    fv_syll_cond = fv_syll;
+end
 
 pitch = [fv_syll_sal(:).mxvals];
 vol = log([fv_syll_sal(:).maxvol]);
 ent = [fv_syll_sal(:).spent];
-tb_sal = jc_tb([fv_syll_sal(:).datenm]',7,0);
-tb_cond = jc_tb([fv_syll_cond(:).datenm]',7,0);
-ind = find(tb_cond<tb_sal(end)+1800); %exclude first half hour of wash in 
+if ~iscell(fv_syll_sal) 
+    tb_sal = jc_tb([fv_syll_sal(:).datenm]',7,0);
+end
+if ~iscell(fv_syll_cond)
+    tb_cond = jc_tb([fv_syll_cond(:).datenm]',7,0);
+end
+if excludewashin == 1
+    ind = find(tb_cond<tb_sal(end)+1800); %exclude first half hour of wash in 
+end
 
 fignum = input('figure number for checking outliers:');
-figure(fignum);hold on;
+figure(fignum);
 plot(tb_sal,pitch,'k.');
 removeoutliers = input('remove outliers (y/n):','s');
 while removeoutliers == 'y'
@@ -58,9 +86,11 @@ cla
 pitch2 = [fv_syll_cond(:).mxvals];
 vol2 = log([fv_syll_cond(:).maxvol]);
 ent2 = [fv_syll_cond(:).spent];
-pitch2(ind) = [];
-vol2(ind) = [];
-ent2(ind) = [];
+if excludewashin == 1
+    pitch2(ind) = [];
+    vol2(ind) = [];
+    ent2(ind) = [];
+end
 
 plot(tb_cond,pitch2,'k.');
 removeoutliers = input('remove outliers:','s');
@@ -105,12 +135,12 @@ while removeoutliers == 'y'
     plot(tb_cond,ent2,'k.');
     removeoutliers = input('remove outliers (y/n):','s');
 end
-hold off; close;
+hold off;
 
 rawplot = input('plot raw summary?:(y/n)','s');
 if rawplot == 'y'
     fignum = input('figure number for fv summary:');
-    figure(fignum);
+    figure(fignum);hold on;
     subtightplot(2,4,1,0.07,0.04,0.1);hold on;
     [hi lo mn1] = mBootstrapCI(pitch);
     plot(0.5,mn1,marker,[0.5 0.5],[hi,lo],linecolor,'linewidth',1,'markersize',12);
@@ -191,10 +221,16 @@ if rawplot == 'y'
     
     subtightplot(2,4,8,0.07,0.04,0.1);hold on;
     [mn1 hi lo] = mBootstrapCI_CV(pitch);
-    plot(0.5,mn1,marker,[0.5 0.5],[hi,lo],linecolor,'linewidth',1,'markersize',12);
+    mn = mn1/mn1;
+    hi = mn+((hi-mn1)/mn1);
+    lo = mn-((mn1-lo)/mn1);
+    plot(0.5,mn,marker,[0.5 0.5],[hi,lo],linecolor,'linewidth',1,'markersize',12);
     [mn2 hi lo] = mBootstrapCI_CV(pitch2);
-    plot(1.5,mn2,marker,[1.5 1.5],[hi lo],linecolor,'linewidth',1,'markersize',12);
-    plot([0.5 1.5],[mn1 mn2],linecolor,'linewidth',1);
+    mn3 = mn2/mn1;
+    hi = mn3+((hi-mn2)/mn1);
+    lo = mn3-((mn2-lo)/mn1);
+    plot(1.5,mn3,marker,[1.5 1.5],[hi lo],linecolor,'linewidth',1,'markersize',12);
+    plot([0.5 1.5],[mn mn3],linecolor,'linewidth',1);
     set(gca,'xlim',[0 2],'xtick',[0.5,1.5],'xticklabel',{'saline','drug'});
     ylabel('Change in pitch CV relative to saline');
     title('Normalized CV changes');
@@ -242,13 +278,20 @@ else
     
     subtightplot(1,4,4,0.07,0.07,0.05);hold on;
     [mn1 hi lo] = mBootstrapCI_CV(pitch);
-    plot(0.5,mn1,marker,[0.5 0.5],[hi,lo],linecolor,'linewidth',1,'markersize',12);
+    mn = mn1/mn1;
+    hi = mn+((hi-mn1)/mn1);
+    lo = mn-((mn1-lo)/mn1);
+    plot(0.5,mn,marker,[0.5 0.5],[hi,lo],linecolor,'linewidth',1,'markersize',12);
     [mn2 hi lo] = mBootstrapCI_CV(pitch2);
-    plot(1.5,mn2,marker,[1.5 1.5],[hi lo],linecolor,'linewidth',1,'markersize',12);
-    plot([0.5 1.5],[mn1 mn2],linecolor,'linewidth',1);
+    mn3 = mn2/mn1;
+    hi = mn3+((hi-mn2)/mn1);
+    lo = mn3-((mn2-lo)/mn1);
+    plot(1.5,mn3,marker,[1.5 1.5],[hi lo],linecolor,'linewidth',1,'markersize',12);
+    plot([0.5 1.5],[mn mn3],linecolor,'linewidth',1);
     set(gca,'xlim',[0 2],'xtick',[0.5,1.5],'xticklabel',{'saline','drug'});
     ylabel('Change in pitch CV relative to saline');
     title('Normalized pitch CV changes for all syllables');
 end
     
 hold off 
+
