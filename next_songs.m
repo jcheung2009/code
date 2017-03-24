4d0f42a2349399852becd3d7e1d19ac7139ac9c1
jcheung <jcheung@macaw.cin.ucsf.edu>
2017-03-23 19:15:55 -0700

    purging

--- next_songs.m
+++
@@ -1,6 +0,0 @@
-cd /net/zebra8/msb/bfw12w31/w12w31.13
-disp('next songs are in /net/zebra8/msb/bfw12w31/w12w31.13/*')
-disp('enter above into upper window and then press filter button')
-disp('then select the file labelled ''batch'' and press ''done''')
-disp('when finished, put cursor in background window and select control c')
-uisonganal3
--- songfile_creation/convert_raw_2_cbin.m
+++
@@ -1,6 +0,0 @@
-function [] = convert_raw_2_cbin(rawfile)
-
-
-[song,fs] = soundin('',char(rawfile),'raw');
-songfile = rawfile(1:end-4);
-make_obs(songfile,song)
