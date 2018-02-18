fec83745f1df1f495d91b4c08ec5705ea8b80d2b
jcheung <jcheung@macaw.cin.ucsf.edu>
2018-02-17 19:54:20 -0800

    gmm spike posteriors

--- RA_neural_analysis_songtiming/spike_posteriors.m
+++ RA_neural_analysis_songtiming/spike_posteriors.m
@@ -1,7 +1,7 @@
 %fit gmm to spike waveforms in each file and extract posterior
 %probabilities for each spike 
 %extract raw neural trace 
-fn = '';
+fn = 'combined_data_B53O71_SU_05_05_2006_1530_1604_PCA_CH0_TH_recommended.mat';
 load(fn);
 
 %% extract waveforms for each spiketime detected for all clusters, maintain
@@ -11,49 +11,62 @@ rawwaveforms = [];
 rawneuraldat = [];
 mainspiketimes = [];mainspikeind = [];
 for i = 1:length(ff)
-    [dat fs] = evsoundin('',ff(i).name,'obs0');
-    load([ff(i).name,'.neuralnot_CH0.mat']);
-    filespiketimes = round([Data.spiketimes{:}]*fs);
+    [dat fs] = evsoundin('',ff(i).name,'obs2');
+    load([ff(i).name,'.neuralnot_CH2.mat']);
+    filespiketimes = round([Data.spiketimes{1:end-1}]*fs);
+    filespiketimes = [filespiketimes round(Data.spiketimes_from_recommended_TH*fs)];
     nsamp = Data.nsamp_wave;
     x = arrayfun(@(x) dat(x-nsamp(1):x+nsamp(2))',filespiketimes,'un',0)';
     rawwaveforms = [rawwaveforms; cell2mat(x)];
-    [~,ia,ib] = intersect(filespiketimes,round(Data.spiketimes_from_recommended_TH*fs));
-    mainspikeind = [mainspikeind;ia];
+    mainspikeind = [mainspikeind;zeros(length([Data.spiketimes{1:end-1}]),1);...
+        ones(length(Data.spiketimes_from_recommended_TH),1)];
     mainspiketimes = [mainspiketimes;length(rawneuraldat)/fs + Data.spiketimes_from_recommended_TH'];
     rawneuraldat = [rawneuraldat;dat];
 end
+assert(length(mainspiketimes)==length(spiketimes))
+assert(isempty(find(ismembertol(mainspiketimes,spiketimes')~=1)))
+assert(length(mainspikeind)==size(rawwaveforms,1))
 
 %% pca waveforms and use scores to fit gmm
+mainspikeind = logical(mainspikeind);
 numclusters = size(Data.spiketimes,2);
-[classes,score] = pca(rawwaveforms);
+[coeff,score] = pca(rawwaveforms);
+
 %plot clusters in first two pca components
 figure;scatter(score(:,1),score(:,2));
 mdl = fitgmdist(score(:,1:2),numclusters);
 [classes,~,p] = cluster(mdl,score(:,1:2));
+
 %plot posteriors in each class 
 figure;hold on;
 for i = 1:numclusters
     [n b] = hist(p(:,i),[0:0.01:1]);hold on;
     stairs(b,n/sum(n));
 end
-%plot average waveform in each class
+
+%plot average waveform in each class, sort clusters by size of waveform
 figure;hold on;
+maxwaveamp = NaN(numclusters,1);
 for i = 1:numclusters
     plot(mean(rawwaveforms(classes==i,:),1));hold on;
+    maxwaveamp(i) = max(abs(mean(rawwaveforms(classes==i,:),1)));
 end
+[~,sortid] = sort(maxwaveamp);
+p = p(:,sortid);
 
-%extract average waveform for main cluster
-mainwaveform = arrayfun(@(x) rawneuraldat(x-nsamp(1):x+nsamp(2))',round(mainspiketimes*fs),'un',0);
+%% extract average waveform for main cluster
+mainwaveform = arrayfun(@(x) rawneuraldat(x-nsamp(1):x+nsamp(2))',round(spiketimes*fs),'un',0)';
 mainwaveform = cell2mat(mainwaveform);
-mainwaveform = mean(mainwaveform,1);
-figure;plot(mainwaveform);
+figure;plot(mean(mainwaveform,1));
 
-%extract posteriors for spikes classified within main cluster
+%% extract posteriors for spikes classified within main cluster
 spikeposterior = p(mainspikeind,:);
+%plot posterior for main cluster for spikes classified under that cluster
 figure;hold on;
-[n b] = hist(spikeposterior(:,3),[0:0.01:1]);
+[n b] = hist(spikeposterior(:,end),[0:0.01:1]);
 stairs(b,n/sum(n));
 assert(size(spikeposterior,1)==length(spiketimes));
 
+%% 
 save(fn,'spikeposterior','mainwaveform','-append')
 
