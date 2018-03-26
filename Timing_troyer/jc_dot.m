0be4a0da674f184454c1d4750d31abc193fbb1f2
jcheung <jcheung@macaw.cin.ucsf.edu>
2018-03-26 15:37:40 -0700

    deleting defunct code

--- Timing_troyer/jc_dot.m
+++
@@ -1,7 +0,0 @@
-function overlap = jc_dot(x,y)
-%another distance measurement that could be used by dtw in Timing_troyer
-%in that case, make sure to use the inverse of the dot product because dtw finds
-%smallest distance 
-x = x(:) *ones(1,size(y,2));
-overlap = dot(x,y,1);
-%overlap = 1./overlap;
--- has_nonalphanum.m
+++
@@ -1,5 +0,0 @@
-function s = has_nonalphanum(str)
-%determine if string contains non-alphanumeric character (excludes
-%underscore)
-
-s = ~isempty(regexp(str,'\W'));
