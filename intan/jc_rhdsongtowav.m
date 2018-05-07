4c6741b6b436334bd049af472a85c67b02495e15
Joanne <jcheung2009@gmail.com>
2018-05-06 19:49:06 -0700

    extract song wav file from rhd files

--- intan/jc_rhdsongtowav.m
+++ intan/jc_rhdsongtowav.m
@@ -1,8 +1,9 @@
 function jc_rhdsongtowav(rhd_data)
+%extract audio channel from rhd files and save as wav file 
 
 for i = 1:length(rhd_data)
     song = rhd_data(i).song/(2*max(abs(rhd_data(i).song)));
-    wavwrite(song,rhd_data(i).Fs,16,[rhd_data(i).filename,'.wav']);
+    audiowrite([rhd_data(i).filename,'.wav'],song,rhd_data(i).Fs);
 end
 
 
